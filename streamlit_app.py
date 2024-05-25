f2a84fc3d4ba0106f3d00aac63d17f0b58cc6c23
pj-azal <jadhavpratik00023@gmail.com>
2024-05-25 06:05:40 +0530

    Update streamlit_app.py
    Update to fix data lookup issues

--- streamlit_app.py
+++ streamlit_app.py
@@ -34,8 +34,9 @@ if ingredients_list:
 
     for fruit_chosen in ingredients_list:
         ingredients_string += fruit_chosen + ' '
+        srch_on = session.table("smoothies.public.fruit_options").filter(col("FRUIT_NAME")==fruit_chosen).select(col('SEARCH_ON'))
         st.subheader(fruit_chosen + 'Nutrition Information')
-        fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_chosen)
+        fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + srch_on)
         fv_df = st.dataframe(data=fruityvice_response.json(), use_container_width=True)
 
     #st.write(ingredients_string)

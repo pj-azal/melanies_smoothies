d796dc343b9d26e4c339dbf7ae2eb2dd95a2c6af
pj-azal <jadhavpratik00023@gmail.com>
2024-05-25 05:39:06 +0530

    Update streamlit_app.py
    Moving code to correct block

--- streamlit_app.py
+++ streamlit_app.py
@@ -34,14 +34,13 @@ if ingredients_list:
 
     for fruit_chosen in ingredients_list:
         ingredients_string += fruit_chosen + ' '
+        fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+        fv_df = st.dataframe(data=fruityvice_response.json(), use_container_width=True)
 
     #st.write(ingredients_string)
 
     my_insert_stmt = """ insert into smoothies.public.orders(ingredients, name_on_order)
             values ('""" + ingredients_string + """', '""" + name_on_order + """')"""
-
-    fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-    fv_df = st.dataframe(data=fruityvice_response.json(), use_container_width=True)
 
     #st.write(my_insert_stmt)
     time_to_insert = st.button('Submit Order')

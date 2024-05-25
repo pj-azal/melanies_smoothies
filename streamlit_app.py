590c414a60a19f8e62d0543cb2d9be120bcbb2af
pj-azal <jadhavpratik00023@gmail.com>
2024-05-25 06:16:27 +0530

    Update streamlit_app.py
    Fix 4

--- streamlit_app.py
+++ streamlit_app.py
@@ -44,8 +44,8 @@ if ingredients_list:
         search_on=pd_df.loc[pd_df['FRUIT_NAME'] == fruit_chosen, 'SEARCH_ON'].iloc[0]
         st.write('The search value for ', fruit_chosen,' is ', search_on, '.')
         #srch_on = session.table("smoothies.public.fruit_options").filter(col("FRUIT_NAME")==fruit_chosen).select(col('SEARCH_ON'))
-        st.subheader(fruit_chosen + 'Nutrition Information')
-        fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + srch_on)
+        st.subheader(fruit_chosen + ' Nutrition Information')
+        fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_chosen)
         fv_df = st.dataframe(data=fruityvice_response.json(), use_container_width=True)
 
     #st.write(ingredients_string)
